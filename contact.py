5b0d83d85afda5f1edf2f27ca1b9626d33be6eb4
OlgaSidortcova <olga-24_@mail.ru>
2020-03-31 18:48:46 +0300

    Выделен класс Application. Методы, кроме тестов, перенесены в Application

---
+++ contact.py
@@ -0,0 +1,12 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, company, address, home_phone, mobile_phone, work_phone, fax, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
