fe4273b1e888089d9e1faa7437af3b2d84811387
OlgaSidortcova <olga-24_@mail.ru>
2020-04-03 12:34:45 +0300

    Выделен вспомогательный класс SessionHelper в файле session.py, через который происходит открытие и закрытие сессии: login и logout. Подправлены тесты: перегруппированы методы перехода по страницам для красоты тестов.

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -13,20 +13,18 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="first name", middlename="middle name", lastname="last name",
                                     company="company", address="address", home_phone="home phone",
                                     mobile_phone="mobile phone",
                                     work_phone="work phone", fax="fax", email="e-mail"))
-    app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
 
 def test_add_contact_empty_phone_and_other(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="first name", middlename="middle name", lastname="last name",
                                     company="company", address="address", home_phone="",
                                     mobile_phone="",
                                     work_phone="", fax="", email=""))
-    app.return_to_home_page()
-    app.logout()
+    app.session.logout()
