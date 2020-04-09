9398e5219d90a3a7f6feab0b2dcf8a7d5cb93358
OlgaSidortcova <olga-24_@mail.ru>
2020-04-09 21:29:53 +0300

    Добавлены 2 теста на модификацию групп. Выделен метод для заполнения поля при изменении знечения в нем.

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -8,3 +8,14 @@ def test_modify_first_group(app):
     app.group.modify_first(Group(name="test111", header="test222", footer="test333"))
     app.session.logout()
 
+
+def test_modify_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first(Group(name="New group"))
+    app.session.logout()
+
+
+def test_modify_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first(Group(header="New header"))
+    app.session.logout()
